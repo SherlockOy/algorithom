48b456fb7f8e410e90f02fa7d3ca3055578ae178
SherlockOy <oyang716@qq.com>
2014-03-19 14:14:29 +0800

    add some annotation

--- leetcode/evalRPN.java
+++ leetcode/evalRPN.java
@@ -10,10 +10,11 @@ public class Solution {
     public int evalRPN(String[] tokens) {
         
         int p = 0;
+        //op2 for the situations of '-' and '/'
         int op2 = 0;
         for(p = 0; p<tokens.length;p++){
             String operator = tokens[p];
-            char type = type(operator);
+            char type = judge(operator);
             switch(type){
                 case '+':
                     pushVal(popVal()+popVal());
@@ -29,9 +30,10 @@ public class Solution {
                     op2 = popVal();
                     pushVal(popVal()/op2);
                     break;
-                case 'n':
+                case 'm':
                     pushVal(Integer.parseInt(operator));
                     break;
+                //default is the positive number
                 default:
                     pushVal(Integer.parseInt(operator));
                     break;
@@ -39,16 +41,18 @@ public class Solution {
         }                   
         return popVal();
     }
-    
-    public char type(String operator){
+    //to judge the type of the operator
+    public char judge(String operator){
         char type = operator.charAt(0);
-        return (operator.length()>1)?'n':type;
+        //'m' represents minus integers
+        return (operator.length()>1)?'m':type;
     }
-    
+    //push the Values into the stack
     public void pushVal(int n){
         values.add(sp, n);
         sp = sp+1;
     }
+    //pop out the top value of the stack
     public int popVal(){
         if(sp>0){
             sp = sp-1;
