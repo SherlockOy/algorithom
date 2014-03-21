948cbc83fea23e6c03efca71736d4246827eb437
SherlockOy <oyang716@qq.com>
2014-03-22 03:12:10 +0800

    add merge sort array and the questions

--- leetcode/BinaryTreeInOrderTraversal.java
+++ leetcode/BinaryTreeInOrderTraversal.java
@@ -1,4 +1,20 @@
 /**
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+
+
  * Definition for binary tree
  * public class TreeNode {
  *     int val;
